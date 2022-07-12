e8529a8a8dddcd3f07fb6bb20d250aad7feb5682
Ernesto Louis <elouis12@me.com>
2022-07-12 01:43:29 -0400

    added mysql stuff

--- config/db.js
+++ config/db.js
@@ -17,18 +17,10 @@ const db = mysql./*connect*/createPool( {
     password: process.env.DB_PASSWORD,
     database: process.env.DB
 
-/*    host: '127.0.0.1',
-    user: 'ernesto',
-    password: '1234567',
-    database: 'geoapp'*/
+
 
 } );
 
 
 module.exports = { db };
 
-
-
-// mysql://b4b1d4f55fe3b1:12bf9b36@us-cdbr-east-06.cleardb.net/heroku_345231a296ca44c?reconnect=true
-//        |              ||       ||                           ||                     |
-//          DB USERNAME   password       host                            DB
